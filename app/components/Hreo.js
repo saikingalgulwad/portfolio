cdf87d6008b909030e238a5e4f8b21aa7845c15a
saikingalgulwad <167662574+saikingalgulwad@users.noreply.github.com>
2025-07-20 19:20:26 +0530

    Update Hreo.js

--- app/components/Hreo.js
+++ app/components/Hreo.js
@@ -9,7 +9,7 @@ function Hreo() {
    <main>
     <div className='mt-3'>
     <h2 className='text-white text-center text-2xl font-bold mt-7 mb-2'>Hello<span className='text-3xl text-amber-400'>.</span></h2>
-    <h3 className='text-white font-mono text-center text-2xl underline decoration-[#ff715b] underline-offset-4'>I'm Saiking</h3>
+    <h3 className='text-white font-mono text-center text-2xl underline decoration-[#ff715b] underline-offset-4'>I'm Saiprasad</h3>
     <h2 className='text-white text-center text-4xl m-6'>Java Developer</h2>
     <div className='flex gap-2 justify-center-safe'>
     <Link className='text-white bg-[#ff715b] inline-block w-[120px] text-center p-1 rounded-[5px]' href={'/projects'}>My Projects</Link>
